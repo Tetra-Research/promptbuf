48c69717c7a100f89cc9892d65ee56a701ee5b2a
Tyler O'Briant <tylerobriant@gmail.com>
2024-10-02 11:40:22 +0100

    Update Tokenizer.tsx

--- marketing/src/components/Tokenizer.tsx
+++ marketing/src/components/Tokenizer.tsx
@@ -140,41 +140,44 @@ interface Model {
 	// 	extend_special_tokens?: Record<string, number>
 	// ) => Tiktoken;
 	enc: Tiktoken;
+	tps: number;
 }
 
 const models: Model[] = [
+	// tps: https://artificialanalysis.ai/leaderboards/models
 	{
 		model: "gpt-3.5-turbo",
 		name: "GPT-3.5 Turbo",
 		enc: encoding_for_model("gpt-4o"),
+		tps: 88.1,
 	},
-	{ model: "gpt-4", name: "GPT-4", enc: encoding_for_model("gpt-4o") },
-	{ model: "gpt-4o", name: "GPT-4o", enc: encoding_for_model("gpt-4o") },
+	{
+		model: "gpt-4",
+		name: "GPT-4",
+		enc: encoding_for_model("gpt-4"),
+		tps: 28.6,
+	},
+	{
+		model: "gpt-4o",
+		name: "GPT-4o",
+		enc: encoding_for_model("gpt-4o"),
+		tps: 124.3,
+	},
 	{
 		model: "gpt-4o-mini",
 		name: "GPT-4o mini",
 		enc: encoding_for_model("gpt-4o"),
+		tps: 105.2,
 	},
 ];
 
 function Tokenizer() {
 	const [selectedExample, setSelectedExample] = useState<Example | null>(null);
 	const [selectedModel, setSelectedModel] = useState<Model | null>(null);
+	const [selectedFormat, setSelectedFormat] = useState<
+		"JSON" | "Typescript" | "Python"
+	>("JSON");
 	const [prettyFormattingEnabled, setPrettyFormattingEnabled] = useState(false);
-
-	// const numValueTokens = enc.encode(
-	// 	JSON.stringify(selectedExample.value)
-	// ).length;
-	// const numEncodedTokens = enc.encode(encoded).length;
-	// const percentReduction =
-	// 	100 * ((numValueTokens - numEncodedTokens) / numValueTokens);
-
-	/*
-	here's what i'm thinking.
-
-	it's a markdown blob.  it has three tabs - json, ts, python
-
-	*/
 
 	const bothSelected = selectedExample && selectedModel;
 
@@ -198,60 +201,97 @@ function Tokenizer() {
 		? 0
 		: Math.round(100 * ((numEncodedTokens - numJSONTokens) / numJSONTokens));
 
+	const latencyReduction = !bothSelected
+		? 0
+		: // tokens * (1 / tokens/ms)
+		  Math.round(
+				(numEncodedTokens - numJSONTokens) * (1 / (selectedModel.tps / 1000))
+		  );
+
 	return (
-		<div className="text-white pt-20 px-40">
-			<div className="space-y-4">
+		<div className="text-white py-4 sm:py-8 md:py-12 lg:py-20 flex w-full items-center justify-center flex-col space-y-8">
+			<div className="space-y-4 w-full px-4 sm:px-6 md:px-8 lg:w-3/4 xl:w-2/3">
 				<div>
-					<div className="flex space-x-2 mb-2 justify-start">
-						<Select
-							value={selectedExample?.id ?? undefined}
-							onValueChange={(id) =>
-								setSelectedExample(examples.find((e) => e.id === id)!)
-							}
-						>
-							<SelectTrigger className="w-[180px]">
-								<SelectValue placeholder="Select an example" />
-							</SelectTrigger>
-							<SelectContent>
-								<SelectGroup>
-									{...examples.map((e) => {
-										return <SelectItem value={e.id}>{e.name}</SelectItem>;
-									})}
-								</SelectGroup>
-							</SelectContent>
-						</Select>
-						<Select
-							value={selectedModel?.model ?? undefined}
-							onValueChange={(model) =>
-								setSelectedModel(models.find((m) => m.model == model)!)
-							}
-						>
-							<SelectTrigger className="w-[180px]">
-								<SelectValue placeholder="Select a model" />
-							</SelectTrigger>
-							<SelectContent>
-								<SelectGroup>
-									{...models.map((m) => {
-										return <SelectItem value={m.model}>{m.name}</SelectItem>;
-									})}
-								</SelectGroup>
-							</SelectContent>
-						</Select>
-						<div className="flex items-center space-x-2">
-							<Switch
-								id="test"
-								checked={prettyFormattingEnabled}
-								onCheckedChange={() =>
-									setPrettyFormattingEnabled(!prettyFormattingEnabled)
+					{/* Controls */}
+					<div className="flex flex-col space-y-4 sm:flex-row sm:justify-between sm:space-y-0">
+						{/* Example Controls */}
+						<div className="flex flex-col space-y-2 sm:flex-row sm:space-x-2 sm:space-y-0">
+							<Select
+								value={selectedExample?.id ?? undefined}
+								onValueChange={(id) =>
+									setSelectedExample(examples.find((e) => e.id === id)!)
 								}
-								defaultChecked={false}
-							/>
-							<Label htmlFor="airplane-mode">Pretty formatted</Label>
+							>
+								<SelectTrigger className="w-full sm:w-[180px]">
+									<SelectValue placeholder="Select an example" />
+								</SelectTrigger>
+								<SelectContent>
+									<SelectGroup>
+										{examples.map((e) => (
+											<SelectItem key={e.id} value={e.id}>
+												{e.name}
+											</SelectItem>
+										))}
+									</SelectGroup>
+								</SelectContent>
+							</Select>
+							<Select
+								value={selectedModel?.model ?? undefined}
+								onValueChange={(model) =>
+									setSelectedModel(models.find((m) => m.model == model)!)
+								}
+							>
+								<SelectTrigger className="w-full sm:w-[180px]">
+									<SelectValue placeholder="Select a model" />
+								</SelectTrigger>
+								<SelectContent>
+									<SelectGroup>
+										{models.map((m) => (
+											<SelectItem key={m.model} value={m.model}>
+												{m.name}
+											</SelectItem>
+										))}
+									</SelectGroup>
+								</SelectContent>
+							</Select>
+							<div className="flex items-center space-x-2">
+								<Switch
+									id="test"
+									checked={prettyFormattingEnabled}
+									onCheckedChange={() =>
+										setPrettyFormattingEnabled(!prettyFormattingEnabled)
+									}
+									defaultChecked={false}
+								/>
+								<Label htmlFor="airplane-mode">Pretty formatted</Label>
+							</div>
+						</div>
+						<div className="mt-4 sm:mt-0">
+							{/* Text controls */}
+							<Select
+								value={selectedFormat}
+								onValueChange={(f: "JSON" | "Typescript" | "Python") =>
+									setSelectedFormat(f)
+								}
+							>
+								<SelectTrigger className="w-full sm:w-auto">
+									<SelectValue placeholder="Select format" />
+								</SelectTrigger>
+								<SelectContent>
+									<SelectGroup>
+										{["JSON", "Typescript", "Python"].map((f) => (
+											<SelectItem key={f} value={f}>
+												{f}
+											</SelectItem>
+										))}
+									</SelectGroup>
+								</SelectContent>
+							</Select>
 						</div>
 					</div>
-					<div className="flex gap-x-4">
+					<div className="flex flex-col md:flex-row gap-4 mt-4">
 						<Textarea
-							className="h-40 resize-none"
+							className="h-40 md:h-48 lg:h-56 resize-none flex-grow"
 							placeholder="Select an example to preview"
 							value={
 								selectedExample?.value
@@ -264,23 +304,11 @@ function Tokenizer() {
 							}
 							readOnly
 						/>
-						{selectedExample && selectedModel && (
-							<div className="text-left">
-								<h3>Tokens</h3>
-								<p>
-									{
-										encoding_for_model(selectedModel.model).encode(
-											JSON.stringify(selectedExample.value)
-										).length
-									}
-								</p>
-							</div>
-						)}
 					</div>
 				</div>
-				<div className="flex gap-x-4">
+				<div className="flex flex-col md:flex-row gap-4">
 					<Textarea
-						className="h-40 resize-none"
+						className="h-40 md:h-48 lg:h-56 resize-none flex-grow"
 						placeholder="Select an example to preview"
 						value={
 							selectedExample
@@ -291,21 +319,60 @@ function Tokenizer() {
 						}
 						readOnly
 					/>
-					{selectedExample && selectedModel && (
+				</div>
+			</div>
+			<div className="w-full px-4 sm:px-6 md:px-8 lg:w-3/4 xl:w-2/3">
+				<div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+					<div className="flex justify-center items-center">
 						<div className="text-left">
-							<h3>Tokens</h3>
-							<p>
-								{
-									encoding_for_model(selectedModel.model).encode(
-										new Promptbuf(selectedExample.schema).encode(
-											selectedExample.value
-										)
-									).length
-								}
+							<h3 className="text-sm sm:text-base lg:text-lg font-semibold">
+								Original Tokens
+							</h3>
+							<p className="text-xl sm:text-2xl lg:text-3xl font-bold">
+								{bothSelected
+									? encoding_for_model(selectedModel.model).encode(
+											JSON.stringify(selectedExample.value)
+									  ).length
+									: "--"}
 							</p>
-							<p className="text-xs">{percentReduction}%</p>
 						</div>
-					)}
+					</div>
+					<div className="flex justify-center items-center">
+						<div className="text-left">
+							<h3 className="text-sm sm:text-base lg:text-lg font-semibold">
+								Encoded Tokens
+							</h3>
+							<p className="text-xl sm:text-2xl lg:text-3xl font-bold">
+								{bothSelected
+									? encoding_for_model(selectedModel.model).encode(
+											new Promptbuf(selectedExample.schema).encode(
+												selectedExample.value
+											)
+									  ).length
+									: "--"}
+							</p>
+						</div>
+					</div>
+					<div className="flex justify-center items-center">
+						<div className="text-left">
+							<h3 className="text-sm sm:text-base lg:text-lg font-semibold">
+								Token Count
+							</h3>
+							<p className="text-xl sm:text-2xl lg:text-3xl font-bold">
+								{bothSelected ? `${percentReduction}%` : "--"}
+							</p>
+						</div>
+					</div>
+					<div className="flex justify-center items-center">
+						<div className="text-left">
+							<h3 className="text-sm sm:text-base lg:text-lg font-semibold">
+								Latency Change
+							</h3>
+							<p className="text-xl sm:text-2xl lg:text-3xl font-bold">
+								{bothSelected ? `${latencyReduction} ms` : "--"}
+							</p>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
